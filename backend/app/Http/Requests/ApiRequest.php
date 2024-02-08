eb4562c6ea9c9f09748ec6ae7e62ce378ec2c483
GTS-MEGHNI <meghniucrpg@live.fr>
2024-02-08 20:16:29 +0100

    Add API Request Class

---
+++ backend/app/Http/Requests/ApiRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ApiRequest extends FormRequest
+{
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'status' => 'error',
+                'message' => 'validation failed',
+                'errors' => $validator->errors(),
+            ])
+        );
+    }
+}
